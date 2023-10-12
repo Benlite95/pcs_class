67ca71cbd7f7e016834dccfea6bbe35323fac52c
slubowsky <270589+slubowsky@users.noreply.github.com>
2023-10-11 20:13:37 -0400

    make function to showDate

--- js/76/clock.js
+++ js/76/clock.js
@@ -1,5 +1,9 @@
 (function () {
   'use strict';
 
-  console.log(new Date().toLocaleDateString(), new Date().toLocaleTimeString());
+  function showDate() {
+    console.log(new Date().toLocaleDateString(), new Date().toLocaleTimeString());
+  }
+  showDate();
+
 }());
