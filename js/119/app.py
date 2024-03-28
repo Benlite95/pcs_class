75311cb79d9828e6698b10e128e28e7696c0e1e2
slubowsky <270589+slubowsky@users.noreply.github.com>
2024-03-27 22:49:22 -0400

    class 119

---
+++ js/119/app.py
@@ -0,0 +1,32 @@
+print('hello world!')
+
+x = 5
+
+print(x, type(x))
+
+x = 'hello'
+
+print(x, type(x))
+
+x = 98.6
+
+print(x, type(x))
+
+x = True
+
+print(x, type(x))
+
+# this is a comment
+
+this_is_a_long_variable_name = 5459843758354574397543985734594377439435873458934574357348795643
+print(this_is_a_long_variable_name, type(this_is_a_long_variable_name))
+
+print(id(this_is_a_long_variable_name))
+
+this_is_a_long_variable_name += 1
+
+print(id(this_is_a_long_variable_name))
+
+x = None
+
+print(x, type(x))
